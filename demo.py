10923eba42d6aa99ecb07475f8ddc06fb4d4415d
CCANT9944 <antohiecristian@gmail.com>
2024-08-01 15:30:48 +0000

    first commit

---
+++ demo.py
@@ -0,0 +1,29 @@
+
+
+
+capt = ["lisbon", "madrid", "paris", "berlin", "london", "viena", "brussels", "amsterdam", "rome", "copenhagen", "oslo", "stockholm", "helsinky", "prague", "warsaw", "athens", "sofia"]
+contr = [""]       
+
+
+class Country:
+    def __init__(self, from_continent):
+        self.from_continent = from_continent
+
+
+    def belongs_to(self):
+        pass
+        
+
+
+
+
+
+class Capital:
+    def __init__(self, from_country):
+        self.from_country = from_country
+
+
+
+
+
+print("Welcome to the History quizz game")
---
+++ gamequizz.py
@@ -0,0 +1,7 @@
+
+import csv
+
+path_file = r"D:\country-list.csv"
+with open ("path_file.csv", "r") as file:
+
+    print(file.read())
